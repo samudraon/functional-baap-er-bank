c299eea3f65d1121f913cd8cce8e96cfbf56317b
samudraon <samudraon@gmailcom>
2023-02-13 00:41:56 +0600

    deposit and utilities section js completed

---
+++ js/deposit.js
@@ -0,0 +1,16 @@
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+
+    /* get previous deposit total by id */
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+
+    /* calculate new deposit total */
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    /* set deposit total value */
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    /* get previous balance by using the function */
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+})
